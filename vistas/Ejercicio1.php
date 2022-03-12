de42300e02afa416e9b6682d71bec2528486e743
CallmeN <95440504+CallmeCarlos503@users.noreply.github.com>
2022-03-11 23:32:42 -0600

    a

--- vistas/Ejercicio1.php
+++ vistas/Ejercicio1.php
@@ -10,7 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 
-<body background="https://wardea.com/wp-content/uploads/2022/01/Yu-Gi-Oh-Master-Duel-4.jpeg">
+<body background="https://coatepec.gob.mx/wp-content/uploads/2019/09/4K-Moving-Stars-Live-Wallpaper-1.jpg">
     <div class="container">
         <br><br>
         <div class="row">
--- vistas/Ejercicio2.php
+++ vistas/Ejercicio2.php
@@ -10,7 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 
-<body background="https://cutewallpaper.org/21/duel-links-wallpaper/Mirror-Wall-Yu-Gi-Oh-Duel-Links-Imitation-Black-Vocaloid-.jpg">
+<body background="https://www.teahub.io/photos/full/54-547089_monster-hunter-portable-3rd.jpg">
     <div class="container">
         <br><br>
         <div class="row">
