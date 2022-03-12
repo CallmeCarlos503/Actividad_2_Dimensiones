85217a5af28449856a43b8c54e9c9645de9ea943
CallmeN <95440504+CallmeCarlos503@users.noreply.github.com>
2022-03-12 13:19:38 -0600

    Update Ejercicio1.php

--- vistas/Ejercicio1.php
+++ vistas/Ejercicio1.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,13 +8,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
-
 <body background="https://coatepec.gob.mx/wp-content/uploads/2019/09/4K-Moving-Stars-Live-Wallpaper-1.jpg">
     <div class="container">
         <br><br>
         <div class="row">
-
-            <div class="col">
+            <div class="col"> 
                 <?php
                 $DATOS[] = [
                     "Enero" => [8, 25, 60],
@@ -80,18 +77,18 @@
                     $mes = $I;
                     foreach ($DATOS[0][$mes] as $A => $AS) {
 
-                        if ($valor != $DATOS[0][$mes][2]) {
+                        if ($valor != $DATOS[0][$mes][2]) { // Calcular promedio de las conservas
                             $valor += $DATOS[0][$mes][2];
+                           
                         }
-                        if ($ValorB < $DATOS[0][$mes][1]) {
+                        if ($ValorB < $DATOS[0][$mes][1]) { // Calcular el valor mayor con su mes
                             $ValorB = $DATOS[0][$mes][1];
                             $mesM = $I;
                         }
-                        if ($mes == "Junio") {
+                        if ($mes == "Junio") {  //Encontrar el menor valor del mes de Junio
                             foreach ($DATOS[0][$mes] as $llave => $value3) {
-                                if ($valorJ > $value3) {
+                                if ($valorJ > $value3) { 
                                     $valorJ = $value3;
-
                                     $Buscador = $llave;
                                     if ($Buscador == 0) {
                                         $producto = "Dulces";
